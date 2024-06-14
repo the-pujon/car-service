811ce60de2073b24d38d9fce86c7c82449fbba0d
Pujon Das Auvi <auvipujondas@gmail.com>
2024-06-14 20:18:16 +0600

    folder structure setup done and eslint and prettier setup done

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express,{ Application,Request,Response } from "express";
+import cors from "cors";
+import httpStatus from "http-status";
+
+const app: Application = express()
+
+app.use(express.json())
+app.use(cors())
+
+let p
+console.log(p)
+
+app.get("/",(req: Request,res: Response) => {
+    res.status(httpStatus.OK).json({
+        status: 'success',
+        message: "Welcome to car washing service api"
+    })
+})
+
+export default app
