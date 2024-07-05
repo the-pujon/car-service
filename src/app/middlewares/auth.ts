a685d49be0ab4e6fcb2a0c646c0ee57ff36660fb
Pujon Das Auvi <auvipujondas@gmail.com>
2024-07-05 11:47:32 +0600

    auth middleware done

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -2,18 +2,40 @@ import { NextFunction, Request, Response } from "express";
 import catchAsync from "../utils/catchAsync.";
 import AppError from "../errors/AppError";
 import httpStatus from "http-status";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import config from "../config";
+import { UserModel } from "../modules/auth/auth.model";
 
-const auth = (requireRole: string) => {
+export const auth = (...requiredRoles: ("admin" | "user")[]) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers.authorization;
+    const token = req.headers.authorization?.split(" ")[1];
 
     //if token can't fount
     if (!token) {
       throw new AppError(httpStatus.UNAUTHORIZED, "You are not authorized");
     }
 
-    const decode = jwt.verify(token, config.jwt_access_secret as string);
+    const decoded = jwt.verify(token, config.jwt_access_secret as string);
+
+    //console.log(decode, requireRole);
+    const { email, role } = decoded as JwtPayload;
+
+    const user = await UserModel.isUserExist(email);
+
+    //if user not found
+    if (!user) {
+      throw new AppError(httpStatus.NOT_FOUND, "This user is not found !");
+    }
+
+    if (requiredRoles && !requiredRoles.includes(role)) {
+      throw new AppError(
+        httpStatus.UNAUTHORIZED,
+        "You are not authorized  hi!",
+      );
+    }
+
+    req.user = decoded as JwtPayload;
+
+    next();
   });
 };
