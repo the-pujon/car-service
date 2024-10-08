e6c4942538001f508c60ba5f591e160336fd1382
Pujon Das Auvi <auvipujondas@gmail.com>
2024-10-08 20:18:26 +0600

    booking model updated

--- src/app/modules/booking/booking.model.ts
+++ src/app/modules/booking/booking.model.ts
@@ -1,53 +1,58 @@
 import { model, Schema } from "mongoose";
 import { TBooking } from "./booking.interface";
 
-const BookingSchema = new Schema<TBooking>({
-  customer: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
+const BookingSchema = new Schema<TBooking>(
+  {
+    customer: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    service: {
+      type: Schema.Types.ObjectId,
+      ref: "Service",
+      required: true,
+    },
+    slot: {
+      type: Schema.Types.ObjectId,
+      ref: "Slot",
+      required: true,
+    },
+    vehicleType: {
+      type: String,
+      enum: [
+        "car",
+        "truck",
+        "SUV",
+        "van",
+        "motorcycle",
+        "bus",
+        "electricVehicle",
+        "hybridVehicle",
+        "bicycle",
+        "tractor",
+      ],
+    },
+    vehicleBrand: {
+      type: String,
+      required: true,
+    },
+    vehicleModel: {
+      type: String,
+      required: true,
+    },
+    manufacturingYear: {
+      type: Number,
+      required: true,
+    },
+    registrationPlate: {
+      type: String,
+      required: true,
+    },
   },
-  service: {
-    type: Schema.Types.ObjectId,
-    ref: "Service",
-    required: true,
+  {
+    timestamps: true,
   },
-  slot: {
-    type: Schema.Types.ObjectId,
-    ref: "Slot",
-    required: true,
-  },
-  vehicleType: {
-    type: String,
-    enum: [
-      "car",
-      "truck",
-      "SUV",
-      "van",
-      "motorcycle",
-      "bus",
-      "electricVehicle",
-      "hybridVehicle",
-      "bicycle",
-      "tractor",
-    ],
-  },
-  vehicleBrand: {
-    type: String,
-    required: true,
-  },
-  vehicleModel: {
-    type: String,
-    required: true,
-  },
-  manufacturingYear: {
-    type: Number,
-    required: true,
-  },
-  registrationPlate: {
-    type: String,
-    required: true,
-  },
-});
+);
 
 export const BookingModel = model<TBooking>("Booking", BookingSchema);
